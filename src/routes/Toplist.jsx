1a02ba59ebba6d63ca8ad72b2215bad41d4a61ae
MalinJ123 <malin--jonsson@hotmail.com>
2023-04-27 15:15:34 +0200

    delat upp innehållet

---
+++ src/routes/Toplist.jsx
@@ -0,0 +1,24 @@
+const Toplist = () => {
+	return (
+		<div className="content">
+			<h2> The toplist </h2>
+			<p>Most searched for books right now!</p>
+			<ol>
+				<li>
+					The Great Gatsby, F. Scott Fitzgerald
+					<button> ❤️ Add to favorites </button>
+				</li>
+				<li>
+					The Great Gatsby, F. Scott Fitzgerald
+					<button> ❤️ Add to favorites </button>
+				</li>
+				<li>
+					The Great Gatsby, F. Scott Fitzgerald
+					<button> ❤️ Add to favorites </button>
+				</li>
+			</ol>
+		</div>
+	);
+};
+
+export default Toplist;
